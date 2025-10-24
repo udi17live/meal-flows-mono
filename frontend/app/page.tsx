39feb5ad21afcb0fbd82fac573b16ce497ec8869
Uditha Mahindarathna <melan.udi@gmail.com>
2025-10-24 11:54:40 +0530

    initial commit - front end

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-9xl">Hello</h1>
+    </div>
+  );
+}
